677def3bc2acf8040b74ca20a23a758b6ef82c97
arkrish1993 <archanarkrishnan3@gmail.com>
2021-06-05 14:27:29 +0530

    using Context Provider and Consumer

--- project-use-effect/src/App.js
+++ project-use-effect/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import AuthContext from "./store/auth-context";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
@@ -25,13 +26,17 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+      }}
+    >
+      <MainHeader onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
