8755b18800eff4ec76bde294def67a12b3b4befa
arkrish1993 <archanarkrishnan3@gmail.com>
2021-06-05 14:52:59 +0530

    creating separate Context component

--- project-use-effect/src/store/auth-context.js
+++ project-use-effect/src/store/auth-context.js
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
+  onLogin: () => {},
   onLogout: () => {},
 });
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    const isAlreadyLoggedIn = localStorage.getItem("isLoggedIn");
+    setIsLoggedIn(isAlreadyLoggedIn === "true");
+  }, []);
+  const loginHandler = () => {
+    localStorage.setItem("isLoggedIn", "true");
+    setIsLoggedIn(true);
+  };
+  const logoutHandler = () => {
+    localStorage.setItem("isLoggedIn", "false");
+    setIsLoggedIn(false);
+  };
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogin: loginHandler,
+        onLogout: logoutHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
