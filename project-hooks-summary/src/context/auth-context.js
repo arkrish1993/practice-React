0724b2212f077a531f1ac50a3673ca22b88dec10
arkrish1993 <archanarkrishnan3@gmail.com>
2021-06-08 20:16:31 +0530

    useContext

---
+++ project-hooks-summary/src/context/auth-context.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+export const AuthContext = React.createContext({
+  isAuth: false,
+  login: () => {},
+});
+
+const AuthContextProvider = (props) => {
+  const [isAuth, setIsAuth] = useState(false);
+  const loginHandler = () => setIsAuth(true);
+  return (
+    <AuthContext.Provider
+      value={{
+        isAuth: isAuth,
+        login: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContextProvider;
