7a82f3e9cedd1b9c8cc0d1fb1d8cbcc8bcacafcb
arkrish1993 <archanarkrishnan3@gmail.com>
2021-06-11 17:38:46 +0530

    Mocking requests instead of sending HTTP requests while testing

--- project-test/src/components/Async.test.js
+++ project-test/src/components/Async.test.js
@@ -3,6 +3,15 @@ import Async from "./Async";
 
 describe("Async wrapper", () => {
   test("Renders posts if request succeeds", async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [
+        {
+          id: "p1",
+          title: "First post.",
+        },
+      ],
+    });
     render(<Async />);
     const listItems = await screen.findAllByRole("listitem");
     expect(listItems).not.toHaveLength(0);
