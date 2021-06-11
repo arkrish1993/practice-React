439fd78fbda74d06724c325cf657d5eca31f4037
arkrish1993 <archanarkrishnan3@gmail.com>
2021-06-11 16:58:55 +0530

    Grouping tests into test suites

--- project-test/src/components/Greeting.test.js
+++ project-test/src/components/Greeting.test.js
@@ -1,14 +1,22 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
 
-test("check if hello world is present", () => {
-  //Arrange
-  render(<Greeting />);
+describe("Greeting component tests", () => {
+  test("Renders hello world", () => {
+    //Arrange
+    render(<Greeting />);
 
-  //Act
-  //...In this case, we don't need to simulate anything like a button click.
+    //Act
+    //...In this case, we don't need to simulate anything like a button click.
 
-  //Assert
-  const isFound = screen.getByText("Hello world", { exact: false });
-  expect(isFound).toBeInTheDocument();
+    //Assert
+    const isFound = screen.getByText("Hello world", { exact: false });
+    expect(isFound).toBeInTheDocument();
+  });
+
+  test("Renders It's good to see you", () => {
+    render(<Greeting />);
+    const isFound = screen.getByText("It's good to see you", { exact: false });
+    expect(isFound).toBeInTheDocument();
+  });
 });
